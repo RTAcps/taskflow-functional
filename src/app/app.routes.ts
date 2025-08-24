df8b45444995fa71112ad5e291aea0c0aaeef5d4
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2025-08-24 17:35:14 -0300

    refactor: adjust routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { ReportDetailComponent } from './features/reports/report-detail.component';
+import { ReportFormComponent } from './features/reports/report-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'dashboard', 
+    pathMatch: 'full' 
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'reports/new',
+    component: ReportFormComponent
+  },
+  {
+    path: 'reports/:id',
+    component: ReportDetailComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
